9b5417eb722b16fdd15c8d4f1e4ce8405319cc4b
Serge Hänni <serge@nyi.ch>
2020-03-29 20:22:33 +0200

    Add skills/projects of versusvirus hackathon

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,22 @@
-ALL_SKILLS = [ 'Biology', 'Biotech', 'Content', 'Design', 'Funding', 'Localization', 'Manufacturing', 'Medicine', 'Mechanics & Electronics', 'PM', 'Operations', 'Software', 'Volunteer vetting', 'Anything' ].freeze
+ALL_SKILLS = [ 'Biology', 'Biotech', 'Content', 'Design', 'Funding', 'Manufacturing', 'Medicine', 'Mechanics & Electronics', 'Project Management', 'Software', 'Volunteer vetting', 'Anything' ,
+  'Allrounder',
+  'Business & Sales',
+  'Craftsmen',
+  'Creative & Arts',
+  'Data wrangling',
+  'Design & Multimedia',
+  'Environmental & Sustainability',
+  'Marketing & PR',
+  'Philanthropic & Humanitarian',
+  'Presenting & Pitching',
+  'Programming',
+  'Project Management',
+  'Service & Gastronomy',
+  'Social & Educational',
+  'Strategy & Business Development',
+  'Medical & Biology',
+  'Sports & Movement'
+].freeze
 ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
 ALL_NUMBER_OF_VOLUNTEERS = [ '1-10', '10-50', '50-100', '100+' ]
 
@@ -16,7 +34,20 @@ ALL_PROJECT_TYPES = [
   'Map volunteers to needs',
   'News and information',
   'Social giving',
-  'Other'
+  'Other',
+  ' Protection of Risk Group',
+  'Dissemination Prevention',
+  'Trustable Data vs. Fake News',
+  'Economical Impact',
+  'Isolation & Mental Health',
+  'Hospital & Medical Care',
+  'Family & Kids',
+  'Education',
+  'Home Office & Teamwork',
+  'After the Crisis',
+  'Support & Solidarity',
+  'Art & Culture',
+  'Crime Reduction'
 ].freeze
 
 VOLUNTEERS_REQUIRED_FOR_FUNDING = 50
